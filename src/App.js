aeb4277c78ce71d9d84fe8961c54e2d11b44f4a0
Rohit Singh Bisht <rb9519208@gmail.com>
2022-03-03 22:49:14 +0530

    updated menu-items with withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* Switch => Only renders the first Route as your first page */}
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
